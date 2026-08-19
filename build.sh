811726c03f8ff7cd4f3405c529cde1582ad9812b
dreamgamer5000 <antonyrejit@gmail.com>
2026-08-20 02:12:45 +0530

    Upload resume PDFs (version 3)

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Compiling resumes..."
-(cd jakes_template_prope && pdflatex -interaction=nonstopmode main.tex > /dev/null)
-(cd 2col && pdflatex -interaction=nonstopmode 2col.tex > /dev/null)
-echo "Done."
